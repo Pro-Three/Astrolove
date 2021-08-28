8fc28147a2bf18941d1d01e0f6cf07b0e6c6ebb9
William Crews <crewswh@gmail.com>
2021-08-28 13:38:31 -0400

    commit

--- krush/src/components/layout/navbar.js
+++ krush/src/components/layout/navbar.js
@@ -7,10 +7,9 @@ const Navbar = () => {
         <div>
         <nav>
             {/* PRE-LOGIN...? */}
-
-            {/* AFTER LOGIN */}
             <div class="nav-wrapper">
             <a href="/" class="brand-logo">AstroLove</a>
+            {/* AFTER LOGIN */}
             <ul id="nav-mobile" class="right hide-on-med-and-down">
                 <li><a href="/chats">Chats</a></li>
                 <li><a href="/profile">Profile</a></li>
