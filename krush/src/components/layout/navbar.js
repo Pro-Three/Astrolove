27a27ac09d96dfec4aa44987bb592b94aeef1e0d
William Crews <crewswh@gmail.com>
2021-08-28 13:16:59 -0400

    ne wcommands

--- krush/src/components/layout/navbar.js
+++ krush/src/components/layout/navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
             <a href="/" class="brand-logo">AstroLove</a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
                 <li><a href="/chats">Chats</a></li>
-                <li><a href="badges.html">Components</a></li>
+                <li><a href="/profile">Profile</a></li>
                 <li><a href="collapsible.html">JavaScript</a></li>
             </ul>
             </div>
