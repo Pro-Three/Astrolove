d712ee4fe5c0d3dd162c69e8c960e245d2e3d880
Lesley Moore <ldmoore15@gmail.com>
2021-08-28 13:28:58 -0400

    login page

---
+++ krush/src/pages/login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div class="container">
+            <div class="row">
+
+            <h3 class="center">Welcome to Astrolove, the newest and hottest dating site! </h3>
+
+            </div>
+        </div>
+    )
+}
+
+export default Login
