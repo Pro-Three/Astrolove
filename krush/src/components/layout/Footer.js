8bf26d162fd7cd8973a9dbada8174f281dfb4e9f
Hamlet Valladares <sigorahv@gmail.com>
2021-08-31 15:54:58 -0400

    fixed footer to stay at the bottom

---
+++ krush/src/components/layout/Footer.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import 'materialize-css';
+
+const styles = {
+footer: {
+    position: 'absolute',
+    left:0,
+    bottom:0,
+    right:0,
+}
+
+}
+
+const Footer = () => {
+    return (
+        <div>
+            <footer class="page-footer #000000 black" style= {styles.footer}>
+                <div class="container">
+                    <div class="row">
+                    <div class="col l6 s12">
+                        <h5 class="white-text">AstroLove</h5>
+                        <p class="grey-text text-lighten-4">Thank you for getting this far!</p>
+                    </div>
+                    <div class="col l4 offset-l2 s12">
+                        <h5 className="white-text">Links</h5>
+                        <ul>
+                        <li><a className="grey-text text-lighten-3" href="/aboutus">About Us</a></li>
+                        <li><a className="grey-text text-lighten-3" href="/contact">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    </div>
+                </div>
+                <div class="footer-copyright">
+                    <div class="container">
+                    © 2021 Copyright Of All Times
+                    <a class="grey-text text-lighten-4 right" href="/">More Links</a>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
