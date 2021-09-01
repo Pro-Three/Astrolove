009239d4e8dd3665bcb54c3bf0dd6022ee66a84c
William Crews <crewswh@gmail.com>
2021-08-31 21:02:10 -0400

    wordlings

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
-// const db = require('./config/connection');
+const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
@@ -17,19 +17,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../krush/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../krush/build')));
+// }
   
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../krush/public/index.html'));
 });
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    // log where we can go to test our GQL API
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
+app.listen(PORT, () => {
+  console.log(`API server running on port ${PORT}!`);
+  // log where we can go to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
+
+// db.once('open', () => {
+//   app.listen(PORT, () => {
+//     console.log(`API server running on port ${PORT}!`);
+//     // log where we can go to test our GQL API
+//     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+//   });
+// });
 
