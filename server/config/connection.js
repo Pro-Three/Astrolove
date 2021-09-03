7234051700e1296ecc151cf47c44fc684b1a6d66
Bryan Civils <hermanbciv@gmail.com>
2021-09-03 10:50:46 -0400

    Rename collection DB to be more descriptive; streamline automation of User seed

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/techmatchup', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/astrolove_db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // useCreateIndex: true,
