955a5b7df8bd5081fa0e1823756f43e5bfabc224
Hamlet Valladares <sigorahv@gmail.com>
2021-09-07 17:12:28 -0400

    Onboard page changed, added css for a clock and css for the website's text

--- krush/src/pages/profile.jsx
+++ krush/src/pages/profile.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
+import '../time.css';
+
+
+  const styles = {
+
+    ul: {
+        width: '800px',
+        margin: '0 auto',
+        padding: '0px',
+        listStyle: 'none',
+        textAlign: 'center'
+    },
+    li: {
+        display: 'inline',
+        fontSize: '10em',
+        textAlign: 'center',
+        textShadow: '0 0 5px #00c6ff'
+    },
+
+    
+}
 
 const Profile = () => {
     return (
-        <div className="container">
-            <div className="row #b0bec5 blue-grey lighten-3">
-
-            <h3 className="center">Your information here.</h3>
-
-            
-
-            </div>
-        </div>
+    <div class="clock">
+    <div id="Date"></div>
+    <ul style= {styles.ul}>
+        <li style= {styles.li} id="hours"></li>
+        <li style= {styles.li} id="point">:</li>
+        <li style= {styles.li} id="min"></li>
+        <li style= {styles.li} id="point">:</li>
+        <li style= {styles.li} id="sec"></li>
+    </ul>
+    <p class="flow-text center-align" id= "Date">It's Virgo Season!!!</p>    
+</div>
     )
 }
 
