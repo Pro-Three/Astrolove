ea600915670b4aba1ccb9dfabeca6d761945a6a0
William Crews <crewswh@gmail.com>
2021-09-07 19:25:14 -0400

    queries

--- krush/src/pages/matches.jsx
+++ krush/src/pages/matches.jsx
@@ -1,17 +1,67 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { useImage } from 'react-image';
+
+function MyImageComponent() {
+    const {src} = useImage({
+      srcList: 'https://cdn.theatlantic.com/media/mt/science/cat_caviar.jpg',
+    })
+
+    return <img src={src} />
+}
 
 const Matches = () => {
     return (
         <div className="container">
             <div className="row grey lighten-3">
+                <h3 className="center">The people you 100%</h3>
+                <div className="col s6 m9 l9">
+                    <Suspense fallback={<div>Loading</div>}>
+                        <MyImageComponent />
+                    </Suspense>
+                </div> 
 
-            <h3 className="center">The people you 100%</h3>
-
-            <h3 className="center">The people you 75%</h3>
-
-            <h3 className="center">The people you 50%-0%</h3>
-
+                <div className="col s12 m3 l3">
+                    <h6>Match Name</h6>
+                    <h6>Match Sign</h6>
+                    <h5>Match Score</h5>
+                </div>
             </div>
+
+            <div className="row grey lighten-3">
+                <h3 className="center">The people you 75%</h3>
+                <div className="col s12 m9 l9">
+                    <Suspense fallback={<div>Loading</div>}>
+                        <MyImageComponent />
+                    </Suspense>
+                </div> 
+
+                <div className="col s12 m3 l3">
+                    <h6>Match Name</h6>
+                    <h6>Match Sign</h6>
+                    <h5>Match Score</h5>
+                </div>
+            </div>
+
+            <div className="row grey lighten-3">
+            <h3 className="center">The people you 50%-0%</h3>
+                <div className="col s12 m9 l9">
+                    <Suspense fallback={<div>Loading</div>}>
+                        <MyImageComponent />
+                    </Suspense>
+                </div> 
+
+                <div className="col s12 m3 l3">
+                    <h6>Match Name</h6>
+                    <h6>Match Sign</h6>
+                    <h5>Match Score</h5>
+                </div>
+            </div>
+
+            
+            
+
+            
+
         </div>
     )
 }
