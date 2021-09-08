f66df331a11b0ed321db2e9bc0b6005157a0f356
William Crews <crewswh@gmail.com>
2021-09-07 20:49:10 -0400

    working

--- krush/src/pages/matches.jsx
+++ krush/src/pages/matches.jsx
@@ -1,6 +1,8 @@
 import React, { Suspense } from 'react';
 import { useImage } from 'react-image';
 
+
+
 function MyImageComponent() {
     const {src} = useImage({
       srcList: 'https://cdn.theatlantic.com/media/mt/science/cat_caviar.jpg',
@@ -13,47 +15,22 @@ const Matches = () => {
     return (
         <div className="container">
             <div className="row grey lighten-3">
-                <h3 className="center">The people you 100%</h3>
-                <div className="col s6 m9 l9">
+                <h3 className="center">Match Name</h3>
+                <div className="col s6 m6 l6">
                     <Suspense fallback={<div>Loading</div>}>
                         <MyImageComponent />
                     </Suspense>
                 </div> 
 
-                <div className="col s12 m3 l3">
-                    <h6>Match Name</h6>
-                    <h6>Match Sign</h6>
-                    <h5>Match Score</h5>
+                <div className="col s3 m3 l3">
+                    <h4>Match Sign:</h4>
+                    <h5 className="center">Leo!</h5>
+                    <h5 className="center">♌️</h5>
+                    <h4>Match Score:</h4>
+                    <h5 className="center">90%</h5>
                 </div>
-            </div>
-
-            <div className="row grey lighten-3">
-                <h3 className="center">The people you 75%</h3>
-                <div className="col s12 m9 l9">
-                    <Suspense fallback={<div>Loading</div>}>
-                        <MyImageComponent />
-                    </Suspense>
-                </div> 
-
-                <div className="col s12 m3 l3">
-                    <h6>Match Name</h6>
-                    <h6>Match Sign</h6>
-                    <h5>Match Score</h5>
-                </div>
-            </div>
-
-            <div className="row grey lighten-3">
-            <h3 className="center">The people you 50%-0%</h3>
-                <div className="col s12 m9 l9">
-                    <Suspense fallback={<div>Loading</div>}>
-                        <MyImageComponent />
-                    </Suspense>
-                </div> 
-
-                <div className="col s12 m3 l3">
-                    <h6>Match Name</h6>
-                    <h6>Match Sign</h6>
-                    <h5>Match Score</h5>
+                <div className="col s3 m3 l3">
+                    <p className="paragraphs">Something about the people like profiles, likes, etc.</p>
                 </div>
             </div>
 
