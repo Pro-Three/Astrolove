5184c417964ad09e6fdbb08acc27ebc6f7b309da
William Crews <crewswh@gmail.com>
2021-09-09 14:46:01 -0400

    workings

--- krush/src/pages/chats.jsx
+++ krush/src/pages/chats.jsx
@@ -95,12 +95,12 @@ import React, { Component } from 'react'
 
 const Chats = () => {
     return (
+        
         <div className="container">
             <div className="row grey lighten-3">
 
             <h3 className="center">You talk here</h3>
-
-            {/* <Demo /> */}
+            
 
             </div>
         </div>
--- krush/src/pages/profile.jsx
+++ krush/src/pages/profile.jsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { useImage } from 'react-image';
 import '../time.css';
+
+function MyImageComponent() {
+    const {src} = useImage({
+      srcList: 'https://cdn.theatlantic.com/media/mt/science/cat_caviar.jpg',
+    })
+
+    return <img src={src} />
+}
 
   const styles = {
 
@@ -18,7 +27,11 @@ import '../time.css';
     },
     profiles: {
         marginBottom: '200px'
+    },
+    otherList: {
+        fontSize: '125%'
     }
+
 
     
 }
@@ -41,26 +54,28 @@ const Profile = () => {
             <div style= {styles.profiles} className="row grey lighten-3 center">
                 <div className="col s12 l12 m12">
                     <h3>Profile Details:</h3>
+                    <Suspense fallback={<div>Loading</div>}>
+                        <MyImageComponent />
+                    </Suspense>
                 </div>
-                <div className="col s6 l6 m6 left" id="Name">
+                <div style= {styles.otherList} className="col s6 l6 m6 left" id="Name">
                     <ul>
                         <li>Name:</li>
                         <li>Sun Sign:</li>
-                        <li>Other</li>
-                        <li>Name:</li>
-                        <li>Other</li>
-                        <li>Other</li>
+                        <li>Gender:</li>
+                        <li>Desired Relationship Preference:</li>
                     </ul>
                 </div>
-                <div className="col s6 l6 m6 left" id="Name">
+                <div style= {styles.otherList} className="col s6 l6 m6 left" id="Name">
                     <ul>
                         <li>User's Name</li>
                         <li>User's Sun Sign</li>
-                        <li>Other</li>
-                        <li>Other</li>
-                        <li>Other</li>
-                        <li>Other</li>
+                        <li>Gender</li>
+                        <li>Desired Relationship Preference</li>
                     </ul>
+                </div>
+                <div className="col s12 m6 l6 center push-l3">
+                    <p>The song came from the bathroom belting over the sound of the shower's running water. It was the same way each day began since he could remember. It listened intently and concluded that the singing today was as terrible as it had ever been.</p>
                 </div>
             </div>
         </div>
@@ -70,3 +85,6 @@ const Profile = () => {
 }
 
 export default Profile
+
+
+// https://randomwordgenerator.com/paragraph.php
