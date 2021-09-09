9a84abed98e7c58c60da93b8ac5e4847bcddd38e
William Crews <crewswh@gmail.com>
2021-09-09 13:57:38 -0400

    working it

--- krush/src/pages/profile.jsx
+++ krush/src/pages/profile.jsx
@@ -16,23 +16,56 @@ import '../time.css';
         textAlign: 'center',
         textShadow: '0 0 5px #00c6ff'
     },
+    profiles: {
+        marginBottom: '200px'
+    }
 
     
 }
 
 const Profile = () => {
     return (
-    <div class="clock">
-    <div id="Date"></div>
-    <ul style= {styles.ul}>
-        <li style= {styles.li} id="hours"></li>
-        <li style= {styles.li} id="point">:</li>
-        <li style= {styles.li} id="min"></li>
-        <li style= {styles.li} id="point">:</li>
-        <li style= {styles.li} id="sec"></li>
-    </ul>
-    <p class="flow-text center-align" id= "Date">It's Virgo Season!!!</p>    
-</div>
+        <div>
+        <div class="clock">
+            <div id="Date"></div>
+                <ul style= {styles.ul}>
+                    <li style= {styles.li} id="hours"></li>
+                    <li style= {styles.li} id="point">:</li>
+                    <li style= {styles.li} id="min"></li>
+                    <li style= {styles.li} id="point">:</li>
+                    <li style= {styles.li} id="sec"></li>
+                </ul>
+            <p class="flow-text center-align" id= "Date">It's Virgo Season!!!</p>    
+        </div>
+        <div className="container">
+            <div style= {styles.profiles} className="row grey lighten-3 center">
+                <div className="col s12 l12 m12">
+                    <h3>Profile Details:</h3>
+                </div>
+                <div className="col s6 l6 m6 left" id="Name">
+                    <ul>
+                        <li>Name:</li>
+                        <li>Sun Sign:</li>
+                        <li>Other</li>
+                        <li>Name:</li>
+                        <li>Other</li>
+                        <li>Other</li>
+                    </ul>
+                </div>
+                <div className="col s6 l6 m6 left" id="Name">
+                    <ul>
+                        <li>User's Name</li>
+                        <li>User's Sun Sign</li>
+                        <li>Other</li>
+                        <li>Other</li>
+                        <li>Other</li>
+                        <li>Other</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        </div>
+
     )
 }
 
