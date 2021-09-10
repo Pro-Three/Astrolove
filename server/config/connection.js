c936e389c7e89a4c9b7f52b76d17730bd5b9d4c6
William Crews <crewswh@gmail.com>
2021-09-10 16:32:54 -0400

    heroku issues

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
+const connection = "mongodb+srv://eqnxcrstpro:NewLife!2021@gyroz.83cg2.mongodb.net/astrolove_db?retryWrites=true&w=majority";
+mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+    .then(() => console.log("Database Connected Successfully"))
+    .catch(err => console.log(err));
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/astrolove_db', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
