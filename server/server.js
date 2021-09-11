5d1bd2162ecaa7089ce94ea2fb662b010c44e32a
William Crews <crewswh@gmail.com>
2021-09-11 10:45:21 -0400

    build problems

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../krush/build')));
 }
   
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../krush/public/index.html'));
 });
 
