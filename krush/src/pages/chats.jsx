751b37ea1df934fe67b4dc1e9da08dce0a44e68c
William Crews <crewswh@gmail.com>
2021-09-11 07:54:00 -0400

    heroku issues and changes to satisfy

--- krush/src/pages/chats.jsx
+++ krush/src/pages/chats.jsx
@@ -10,13 +10,13 @@ const Chats = () => {
 
 <div className="container cyan accent-4">
     <div class="menu">
-          <div class="back"><i class="fa fa-chevron-left"></i> <img src={me} draggable="false"/></div>
+          <div class="back"><i class="fa fa-chevron-left"></i> <img src={me} draggable="false" alt="image, but pronounced 'em-aage'"/></div>
             <div class="name">Hamlet</div>
         </div>
 
     <ol class="chat">
     <li class="other">
-        <div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false"/></div>
+        <div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false" alt="image, but pronounced 'em-aage'"/></div>
       <div class="msg">
         <p>Hey, how are you?</p>
         <p>Does your horoscope like pizza?<emoji class="pizza"/></p>
@@ -24,7 +24,7 @@ const Chats = () => {
       </div>
     </li>
     <li class="self">
-        <div class="avatar"><img src={me2} draggable="false"/></div>
+        <div class="avatar"><img src={me2} draggable="false" alt="image, but pronounced 'em-aage'"/></div>
       <div class="msg">
         <h6 style={{textDecoration: 'underline'}}>William</h6>
         <p>Hey what's up!</p>
@@ -34,14 +34,14 @@ const Chats = () => {
       </div>
     </li>
     <li class="other">
-        <div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false"/></div>
+        <div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false" alt="image, but pronounced 'em-aage'"/></div>
       <div class="msg">
         <p>Guess what my horoscope said today??? <emoji class="suffocated"/></p>
         <time>September 8th, 2021</time>
       </div>
     </li>
     <li class="self">
-        <div class="avatar"><img src="https://i.imgur.com/HYcn9xO.png" draggable="false"/></div>
+        <div class="avatar"><img src="https://i.imgur.com/HYcn9xO.png" draggable="false" alt="image, but pronounced 'em-aage'"/></div>
       <div class="msg">
       <h6 style={{textDecoration: 'underline'}}>William</h6>
 
@@ -50,7 +50,7 @@ const Chats = () => {
       </div>
     </li>
     <li class="other">
-        <div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false"/></div>
+        <div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false" alt="image, but pronounced 'em-aage'"/></div>
       <div class="msg">
         <p><emoji class="scream"/></p>
         <p>That a virgo was going to come and ruin my life<emoji class="please"/></p>
@@ -58,7 +58,7 @@ const Chats = () => {
       </div>
     </li>
     <li class="self">
-        <div class="avatar"><img src="https://i.imgur.com/HYcn9xO.png" draggable="false"/></div>
+        <div class="avatar"><img src="https://i.imgur.com/HYcn9xO.png" draggable="false" alt="image, but pronounced 'em-aage'"/></div>
       <div class="msg">
         <img src="https://i.imgur.com/QAROObc.jpg" draggable="false"/>
         <h6 style={{textDecoration: 'underline'}}>William</h6>
@@ -69,7 +69,7 @@ const Chats = () => {
       </div>
     </li>
     <li class="other">
-        <div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false"/></div>
+        <div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false" alt="image, but pronounced 'em-aage'"/></div>
       <div class="msg">
         {/* <p>Gracias! <emoji class="hearth_blue"/></p> */}
         <p>Mercury is in retrograde be safe out there bro</p>
