6a41b4083fbda4b23049642551d29d7325bba23d
William Crews <crewswh@gmail.com>
2021-09-11 08:54:26 -0400

    heroku errors, but not 404

--- server/server.js
+++ server/server.js
@@ -20,13 +20,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "krush/build")));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../krush/build')));
 }
   
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "krush/build", "index.html"));
-});
+  res.sendFile(path.join(__dirname, '../krush/public/index.html'));
+}); 
 
 app.use((req, res, next) => {
   const error = new Error("Not found");
